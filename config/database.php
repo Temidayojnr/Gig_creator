dd740a32dcc3b04db0985bd71660bef5bfde33c2
Temidayojnr <temidayojunior@gmail.com>
2020-07-10 10:57:28 +0100

    DB config added

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$host = $url["host"]?? null;
+$username = $url["user"]?? null;
+$password = $url["pass"]?? null;
+$database = substr($url["path"], 1)?? null;
+
 return [
 
     /*
@@ -15,7 +21,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_prod'),
 
     /*
     |--------------------------------------------------------------------------
@@ -76,6 +82,17 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+
+        'pgsql_prod' =>[
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
         'sqlsrv' => [
